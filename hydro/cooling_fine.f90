8df8dfd1b80cd47bc8f7fe6e5685f47f87c5eea5
Patrick Hennebelle <phennebe@alfven.cluster>
2021-09-04 18:26:03 +0200

    finish rho_floor option by recalculating also total energy

--- hydro/cooling_fine.f90
+++ hydro/cooling_fine.f90
@@ -176,9 +176,14 @@ subroutine coolfine1(ind_grid,ngrid,ilevel)
         ! If density is below the threshold, correct it
         if(rho_floor .and. uold(ind_leaf(i), 1) < smallr) then
            do idim = 1, 3
+              uold(ind_leaf(i), 5) = uold(ind_leaf(i), 5) - 0.5*uold(ind_leaf(i), idim + 1)**2 / abs(uold(ind_leaf(i), 1))
               uold(ind_leaf(i), idim + 1) = (uold(ind_leaf(i), idim + 1) * smallr / abs(uold(ind_leaf(i), 1)))
            enddo
+           uold(ind_leaf(i), 5) = abs(uold(ind_leaf(i),5)) * smallr / abs(uold(ind_leaf(i), 1))
            uold(ind_leaf(i), 1) = smallr
+           do idim = 1, 3
+              uold(ind_leaf(i), 5) = uold(ind_leaf(i), 5) + 0.5*uold(ind_leaf(i), idim + 1)**2 / abs(uold(ind_leaf(i), 1))
+           enddo
         endif
 !!! NBrucy
         nH(i)=MAX(uold(ind_leaf(i),1),smallr)
