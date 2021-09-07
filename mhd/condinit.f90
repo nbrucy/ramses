1e3dd6abe1949c192f2d7fc11e4d0526bca6841b
Tine Colman <tinecolman@hotmail.com>
2021-09-07 17:33:00 +0200

    fix mhd condinit variable indices

--- mhd/condinit.f90
+++ mhd/condinit.f90
@@ -15,14 +15,19 @@ subroutine condinit_default(x,u,dx,nn)
   ! Positions are in user units:
   ! x(i,1:3) are in [0,boxlen]**ndim.
   ! U is the conservative variable vector. Conventions are here:
-  ! U(i,1): d, U(i,2:ndim+1): d.u,d.v,d.w and U(i,ndim+2): E.
+  ! U(i,1): d, U(i,2:4): d.u,d.v,d.w, U(i,5): E, U(i,6:8): Bleft,
+  ! U(i,nvar+1:nvar+3): Bright
   ! Q is the primitive variable vector. Conventions are here:
-  ! Q(i,1): d, Q(i,2:ndim+1):u,v,w and Q(i,ndim+2): P.
-  ! If nvar >= ndim+3, remaining variables are treated as passive
+  ! Q(i,1): d, Q(i,2:4):u,v,w, Q(i,5): P, Q(i,6:8): Bleft,
+  ! Q(i,nvar+1:nvar+3): Bright
+  ! If nvar > 8, remaining variables (9:nvar) are treated as passive
   ! scalars in the hydro solver.
   ! U(:,:) and Q(:,:) are in user units.
   !================================================================
-#if NENER>0 || NVAR>NDIM+2+NENER
+#if NENER>0
+  integer::irad
+#endif
+#if NVAR>8+NENER
   integer::ivar
 #endif
   real(dp),dimension(1:nvector,1:nvar+3),save::q   ! Primitive variables
@@ -35,46 +40,48 @@ subroutine condinit_default(x,u,dx,nn)
   u(1:nn,1)=q(1:nn,1)
   ! velocity -> momentum
   u(1:nn,2)=q(1:nn,1)*q(1:nn,2)
-#if NDIM>1
   u(1:nn,3)=q(1:nn,1)*q(1:nn,3)
-#endif
-#if NDIM>2
   u(1:nn,4)=q(1:nn,1)*q(1:nn,4)
-#endif
   ! kinetic energy
-  u(1:nn,ndim+2)=0.0d0
-  u(1:nn,ndim+2)=u(1:nn,ndim+2)+0.5d0*q(1:nn,1)*q(1:nn,2)**2
-#if NDIM>1
-  u(1:nn,ndim+2)=u(1:nn,ndim+2)+0.5d0*q(1:nn,1)*q(1:nn,3)**2
-#endif
-#if NDIM>2
-  u(1:nn,ndim+2)=u(1:nn,ndim+2)+0.5d0*q(1:nn,1)*q(1:nn,4)**2
-#endif
-  ! thermal pressure -> total fluid energy
-  u(1:nn,ndim+2)=u(1:nn,ndim+2)+q(1:nn,ndim+2)/(gamma-1.0d0)
+  u(1:nn,5)=0.0d0
+  u(1:nn,5)=u(1:nn,5)+0.5*q(1:nn,1)*q(1:nn,2)**2
+  u(1:nn,5)=u(1:nn,5)+0.5*q(1:nn,1)*q(1:nn,3)**2
+  u(1:nn,5)=u(1:nn,5)+0.5*q(1:nn,1)*q(1:nn,4)**2
+  ! pressure -> total fluid energy
+  u(1:nn,5)=u(1:nn,5)+q(1:nn,5)/(gamma-1.0d0)
+  ! magnetic energy -> total fluid energy
+  u(1:nn,5)=u(1:nn,5)+0.125d0*(q(1:nn,6)+q(1:nn,nvar+1))**2
+  u(1:nn,5)=u(1:nn,5)+0.125d0*(q(1:nn,7)+q(1:nn,nvar+2))**2
+  u(1:nn,5)=u(1:nn,5)+0.125d0*(q(1:nn,8)+q(1:nn,nvar+3))**2
+  u(1:nn,6:8)=q(1:nn,6:8)
+  u(1:nn,nvar+1:nvar+3)=q(1:nn,nvar+1:nvar+3)
 #if NENER>0
   ! radiative pressure -> radiative energy
   ! radiative energy -> total fluid energy
-  do ivar=1,nener
-     u(1:nn,ndim+2+ivar)=q(1:nn,ndim+2+ivar)/(gamma_rad(ivar)-1.0d0)
-     u(1:nn,ndim+2)=u(1:nn,ndim+2)+u(1:nn,ndim+2+ivar)
+  do irad=1,nener
+     u(1:nn,8+irad)=q(1:nn,8+irad)/(gamma_rad(irad)-1.0d0)
+     u(1:nn,5)=u(1:nn,5)+u(1:nn,8+irad)
   enddo
 #endif
-
-!!!!!!!
-write(*,*) 'take care with magnetized condinit_default...'
-
-#if NVAR>NDIM+2+NENER
+#if NVAR>8+NENER
   ! passive scalars
-  do ivar=ndim+3+nener,nvar
+  do ivar=9+nener,nvar
      u(1:nn,ivar)=q(1:nn,1)*q(1:nn,ivar)
   end do
 #endif
 
 end subroutine condinit_default
 
-
 subroutine condinit(x,u,dx,nn)
+  use hydro_parameters
+  use amr_commons
+  implicit none
+  ! amr data
+  integer ::nn                            ! Number of cells
+  real(dp)::dx                            ! Cell size
+  real(dp),dimension(1:nvector,1:nvar+3)::u ! Conservative variables
+  real(dp),dimension(1:nvector,1:ndim)::x ! Position of cell center
+  logical,save:: first_call = .true.           ! True if this is the first call to condinit
 
   !================================================================
   ! This routine generates initial conditions for RAMSES.
@@ -88,17 +95,6 @@ subroutine condinit(x,u,dx,nn)
   ! scalars in the hydro solver.
   ! U(:,:) and Q(:,:) are in user units.
   !================================================================
-  use hydro_parameters
-  use amr_commons
-  implicit none
-
-  ! amr data
-  integer ::nn                            ! Number of cells
-  real(dp)::dx                            ! Cell size
-  real(dp),dimension(1:nvector,1:nvar+3)::u ! Conservative variables
-  real(dp),dimension(1:nvector,1:ndim)::x ! Position of cell center
-  logical,save:: first_call = .true.           ! True if this is the first call to condinit
-
   select case (condinit_kind)
 
   case('galbox')
@@ -119,10 +115,6 @@ subroutine condinit(x,u,dx,nn)
   first_call = .false.
 
 end subroutine condinit
-
-
-
-
 
 subroutine boundary_frig(ilevel)
   use hydro_parameters
@@ -153,3 +145,74 @@ subroutine boundary_frig(ilevel)
 
 
 end subroutine boundary_frig
+
+subroutine velana(x,v,dx,t,ncell)
+  use amr_parameters
+  use hydro_parameters
+  implicit none
+  integer ::ncell                         ! Size of input arrays
+  real(dp)::dx                            ! Cell size
+  real(dp)::t                             ! Current time
+  real(dp),dimension(1:nvector,1:3)::v    ! Velocity field
+  real(dp),dimension(1:nvector,1:ndim)::x ! Cell center position.
+  !================================================================
+  ! This routine computes the user defined velocity fields.
+  ! x(i,1:ndim) are cell center position in [0,boxlen] (user units).
+  ! v(i,1:3) is the imposed 3-velocity in user units.
+  !================================================================
+  integer::i
+  real(dp)::xx,yy=0.,zz=0.,vx,vy,vz,aa,twopi
+!!$  real(dp)::rr,tt,omega
+  
+  ! Add here, if you wish, some user-defined initial conditions
+  aa=1.0+0.*t
+  twopi=2d0*ACOS(-1d0)
+  do i=1,ncell
+
+     xx=x(i,1)+0.*dx
+#if NDIM > 1
+     yy=x(i,2)
+#endif
+#if NDIM > 2
+     zz=x(i,3)
+#endif
+     ! ABC
+     vx=aa*(cos(twopi*yy)+sin(twopi*zz))
+     vy=aa*(sin(twopi*xx)+cos(twopi*zz))
+     vz=aa*(cos(twopi*xx)+sin(twopi*yy))
+
+!!$     ! 1D advection test
+!!$     vx=1.0_dp
+!!$     vy=0.0_dp
+!!$     vz=0.0_dp
+
+!!$     ! Ponomarenko
+!!$     xx=xx-boxlen/2.0
+!!$     yy=yy-boxlen/2.0
+!!$     rr=sqrt(xx**2+yy**2)
+!!$     if(yy>0)then
+!!$        tt=acos(xx/rr)
+!!$     else
+!!$        tt=-acos(xx/rr)+twopi
+!!$     endif
+!!$     if(rr<1.0)then
+!!$        omega=0.609711
+!!$        vz=0.792624
+!!$     else
+!!$        omega=0.0
+!!$        vz=0.0
+!!$     endif
+!!$     vx=-sin(tt)*rr*omega
+!!$     vy=+cos(tt)*rr*omega
+
+     v(i,1)=vx
+#if NDIM > 1
+     v(i,2)=vy
+#endif
+#if NDIM > 2
+     v(i,3)=vz
+#endif
+  end do
+
+
+end subroutine velana
