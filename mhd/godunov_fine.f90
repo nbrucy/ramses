efb5cf90189320b0b3961135d0352c974052bed5
Noe Brucy <noe.brucy@cea.fr>
2021-09-07 14:48:25 +0200

    add rho_floor patch to set a minimal density value equal to smallr

--- mhd/godunov_fine.f90
+++ mhd/godunov_fine.f90
@@ -188,11 +188,11 @@ subroutine set_uold(ilevel)
   use poisson_commons
   implicit none
   integer::ilevel
-  !---------------------------------------------------------
-  ! This routine sets array uold to its new value unew
-  ! after the hydro step.
-  !---------------------------------------------------------
-  integer::i,ivar,ind,iskip,nx_loc,ind_cell
+  !--------------------------------------------------------------------------
+  ! This routine sets array uold to its new value unew after the
+  ! hydro step.
+  !--------------------------------------------------------------------------
+  integer::i,ivar,ind,iskip,nx_loc,ind_cell,idim
   real(dp)::scale,d,u,v,w,A,B,C
   real(dp)::e_mag,e_kin,e_cons,e_prim,e_trunc,div,dx
 #if NENER>0
@@ -205,6 +205,32 @@ subroutine set_uold(ilevel)
   nx_loc=icoarse_max-icoarse_min+1
   scale=boxlen/dble(nx_loc)
   dx=0.5d0**ilevel*scale
+
+!!! BrucyN - rho_floor
+  ! If density is below the threshold, correct it
+  if(rho_floor) then
+   do ind=1,twotondim
+      iskip=ncoarse+(ind-1)*ngridmax
+      do i=1,active(ilevel)%ngrid
+         ind_cell=active(ilevel)%igrid(i)+iskip
+         if(unew(ind_cell, 1) < smallr) then
+            do idim = 1, 3
+               unew(ind_cell, 5) = unew(ind_cell, 5) - 0.5*unew(ind_cell, idim + 1)**2 / abs(unew(ind_cell, 1))
+               unew(ind_cell, 5) = unew(ind_cell, 5) - 0.125*(unew(ind_cell,5 + idim) + unew(ind_cell,nvar + idim))**2
+               unew(ind_cell, idim + 1) = (unew(ind_cell, idim + 1) * smallr / abs(unew(ind_cell, 1)))
+            end do
+            unew(ind_cell, 5) = abs(unew(ind_cell,5)) * smallr / abs(unew(ind_cell, 1))
+            unew(ind_cell, 1) = smallr
+            do idim = 1, 3
+               unew(ind_cell, 5) = unew(ind_cell, 5) + 0.5*unew(ind_cell, idim + 1)**2 / abs(unew(ind_cell, 1))
+               unew(ind_cell, 5) = unew(ind_cell, 5) + 0.125*(unew(ind_cell, 5 + idim) + unew(ind_cell, nvar + idim))**2
+            end do
+         end if
+      end do
+   end do
+  end if
+!!! NBrucy
+
 
   ! Set uold to unew for myid cells
   do ind=1,twotondim
@@ -262,7 +288,7 @@ subroutine set_uold(ilevel)
            end if
         end do
      end if
-  end do
+   end do
 
 111 format('   Entering set_uold for level ',i2)
 
