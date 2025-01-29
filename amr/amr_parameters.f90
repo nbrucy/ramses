2b8cd9ea91095e51f020f8682cf91e991ad9e848
Noe Brucy <noe.brucy@cea.fr>
2025-01-29 16:34:21 +0100

    add special boundary framework

--- amr/amr_parameters.f90
+++ amr/amr_parameters.f90
@@ -333,6 +333,9 @@ module amr_parameters
   integer ,dimension(1:MAXBOUND)    ::kbound_min=0
   integer ,dimension(1:MAXBOUND)    ::kbound_max=0
   logical                           ::no_inflow=.false.
+  logical                          ::use_special_boundary=.false.
+  character(LEN=60)                 ::special_boundary_kind ='none'
+
 
   ! Number of processes sharing one token
   ! Only one process can write at a time in an I/O group
