bf29e27288a9037b78915bfb3b03d4a36e407f6d
Noé Brucy <noe.brucy@ens-lyon.fr>
2026-08-04 17:37:06 +0200

    fix CIC

--- pm/move_fine.f90
+++ pm/move_fine.f90
@@ -493,7 +493,7 @@ subroutine move1(ind_grid,ind_part,ind_grid_part,ng,np,ilevel)
         end do
 #ifdef OUTPUT_PARTICLE_POTENTIAL
         do j=1,np
-           ptcl_phi(ind_part(j)) = ptcl_phi(ind_part(j))  + phi(indp(j, ind))*vol(j,ind)/vol_loc
+           ptcl_phi(ind_part(j)) = ptcl_phi(ind_part(j)) + phi(indp(j, ind))*vol(j,ind)
         end do
 #endif
      end do
